8af4531db6585ef57d749e1d9ff0fc662343a83d
David Cavins <david.cavins@gmail.com>
2016-08-08 17:02:40 -0500

    Create AJAX action for saving a leaders report as a doc.
    Add sample file showing data required and handling the response.

--- public/class-cc-salud-america.php
+++ public/class-cc-salud-america.php
@@ -183,6 +183,9 @@ class CC_Salud_America {
 		// Welcome email. Filter the content for SA groups.
 		add_filter( 'ass_welcome_email', array( $this, 'customize_welcome_email' ), 20, 3 );
 
+		// Save SA leader reports to the docs library
+		add_action( 'wp_ajax_save-leader-report-as-doc', array( $this, 'ajax_save_leader_report_as_doc' ) );
+
 	}
 
 
@@ -1866,6 +1869,93 @@ class CC_Salud_America {
 		}
 
 		return $message_data;
+	}
+
+	/**
+	 * Create a report-style doc via AJAX from the report card tab.
+	 *
+	 * @since 1.8.0
+	 */
+	public function ajax_save_leader_report_as_doc() {
+		// Do our dependencies exist?
+		if ( ! class_exists( 'CC_MRAD' ) || ! function_exists( 'bp_docs_get_post_type_name' ) ) {
+			wp_send_json_error( 'Software dependencies are not met.' );
+		}
+
+		// Is the nonce any good?
+		check_ajax_referer( 'save-leader-report-' . bp_loggedin_user_id() );
+
+		// Can the user do this?
+		if ( ! current_user_can( 'bp_docs_associate_with_group', sa_get_group_id() ) ) {
+			wp_send_json_error( 'User is not allowed to take this action.' );
+		}
+
+		// Sanitize incoming POST data.
+		$county = $state = $geoid = null;
+		if ( ! empty( $_POST['county'] ) ) {
+			$county = sanitize_text_field( $_POST['county'] );
+		}
+		if ( ! empty( $_POST['state'] ) ) {
+			$state = sanitize_text_field( $_POST['state'] );
+		}
+		if ( ! empty( $_POST['geoid'] ) ) {
+			// These are county level and take the form 05000US05013
+			if ( preg_match( "/^05000US([0-9]{5})$/i", $_POST['geoid'] ) ) {
+				$geoid = $_POST['geoid'];
+			}
+		}
+		if ( empty( $geoid ) ) {
+			wp_send_json_error( 'A location must be specified and be of the correct format.' );
+		}
+
+		// First, check to see if this user has already "saved" this exact report.
+		$dupe_check_args = array(
+			'post_type'  => bp_docs_get_post_type_name(),
+			'author'     => bp_loggedin_user_id(),
+			'meta_key'   => 'geoid',
+			'meta_value' => $geoid,
+			'fields'     => 'ids',
+		);
+		$dupes = new WP_Query( $dupe_check_args );
+		if ( $dupes->posts ) {
+			// Return the post ID. Should this be a success or a failure?
+			wp_send_json_success( current( $dupes->posts ) );
+		}
+
+		if ( $county && $state ) {
+			$title = "Salud America! Leaders Report for {$county}, {$state}";
+		} else {
+			$title = 'Salud America! Leaders Report';
+		}
+
+		$content = '<a href="/groups/salud-america/report-card/?geoid=' . $geoid . '" title="Link to report" class="button report-link"><span class="icon reportx24"></span>View Report Card</a>';
+
+		$args = array(
+			'title'			=> $title,
+			'content' 		=> $content,
+			'author_id'     => bp_loggedin_user_id(),
+			'group_id'		=> sa_get_group_id(),
+			'is_auto'		=> 0,
+			'settings'		=> array(   'read' => 'group-members',
+										'edit' => 'creator',
+										'read_comments' => 'group-members',
+										'post_comments' => 'group-members',
+										'view_history' => 'creator'
+									),
+			'parent_id'		=> 0,
+		);
+
+		$instance = new CC_MRAD_BP_Doc_Save;
+		$post_id = $instance->save( $args );
+
+		if ( $post_id ) {
+			$mrad_class = new CC_MRAD;
+			wp_set_object_terms( $post_id, 'report', $mrad_class->get_taxonomy_name() );
+			// Set the GeoID as post_meta so we can check for uniqueness in the future.
+			add_post_meta( $post_id, 'geoid', $geoid );
+		}
+
+		wp_send_json_success( $post_id );
 	}
 }
 $cc_salud_america = new CC_Salud_America();
