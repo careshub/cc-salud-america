3ab4c364f4e6dbecded3a99d251cf0a6d6995dec
David Cavins <david.cavins@gmail.com>
2016-08-15 08:56:11 -0500

    Bump version; create minified assets.

--- cc-salud-america.php
+++ cc-salud-america.php
@@ -15,7 +15,7 @@
  * Plugin Name:       CC Salud America
  * Plugin URI:        @TODO
  * Description:       Adds SA functionality to CC site
- * Version:           1.7.0
+ * Version:           1.8.0
  * Author:            David Cavins
  * Author URI:        @TODO
  * Text Domain:       plugin-name-locale
--- public/class-cc-salud-america.php
+++ public/class-cc-salud-america.php
@@ -29,7 +29,7 @@ class CC_Salud_America {
 	 *
 	 * @var     string
 	 */
-	const VERSION = '1.7.1';
+	const VERSION = '1.8.0';
 
 	/**
 	 *
